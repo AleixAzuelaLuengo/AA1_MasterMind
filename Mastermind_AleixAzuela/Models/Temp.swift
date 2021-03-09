e62a111099a616ca61f24014a648ee2892874c65
Aleix <aleix.azuela@enti.cat>
2021-03-09 19:19:37 +0100

    Initial Commit

---
+++ Mastermind_AleixAzuela/Models/Temp.swift
@@ -0,0 +1,8 @@
+//
+//  Temp.swift
+//  Mastermind_AleixAzuela
+//
+//  Created by Alumne on 9/3/21.
+//
+
+import Foundation
---
+++ Mastermind_AleixAzuela/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  Mastermind_AleixAzuela
+//
+//  Created by Alumne on 9/3/21.
+//
+
+import Foundation
+
+class ViewModel
+{
+    
+}
