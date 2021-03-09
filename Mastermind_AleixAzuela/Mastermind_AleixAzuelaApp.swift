7a7ca2994b559b80e54e703f65ba1fae52c1f91d
Aleix <aleix.azuela@enti.cat>
2021-03-09 18:55:02 +0100

    Initial Commit

---
+++ Mastermind_AleixAzuela/Mastermind_AleixAzuelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mastermind_AleixAzuelaApp.swift
+//  Mastermind_AleixAzuela
+//
+//  Created by Alumne on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Mastermind_AleixAzuelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
